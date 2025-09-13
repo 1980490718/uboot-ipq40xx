163bcdcfe8cc310debfaa0c92e4655aa675690f2
Willem Lee <1980490718@qq.com>
2025-09-14 00:23:51 +0800

    Update MTDPARTS and BOOT_MTDPARTS for R619AC and e2600acc2

--- uboot/include/configs/ipq40xx_e2600acc2.h
+++ uboot/include/configs/ipq40xx_e2600acc2.h
@@ -5,6 +5,10 @@
 #ifndef _IPQ40XX_E2600ACC2_H
 #define _IPQ40XX_E2600ACC2_H
 
+#define MTDIDS              "nand0=nand0"
+#define MTDPARTS            "mtdparts=nand0:-(ubi)"
+#define BOOT_MTDPARTS       "mtdparts=nand0:0x4000000@0(ubi)"
+
 #include <configs/ipq40xx_cdp.h>
 #include <ipq40xx_api.h>
 
--- uboot/include/configs/ipq40xx_r619ac.h
+++ uboot/include/configs/ipq40xx_r619ac.h
@@ -5,6 +5,10 @@
 #ifndef _IPQ40XX_R619AC_H
 #define _IPQ40XX_R619AC_H
 
+#define MTDIDS              "nand0=nand0"
+#define MTDPARTS            "mtdparts=nand0:-(rootfs)"
+#define BOOT_MTDPARTS       "mtdparts=nand0:0x4000000@0(rootfs)"
+
 #include <configs/ipq40xx_cdp.h>
 #include <ipq40xx_api.h>
 
